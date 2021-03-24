b6137c4612aa37941774e3c44a0dd975cfe0f958
fmateus <fm4teus@gmail.com>
2021-03-24 13:41:52 -0300

    read words, letter_scores and export json

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import json
 
+import unicodedata
+def strip_accents(s):
+   return ''.join(c for c in unicodedata.normalize('NFD', s)
+                  if unicodedata.category(c) != 'Mn')
 
 letters_filename = 'letters.txt'
 words_filename = 'words.txt'
@@ -16,20 +20,22 @@ def read_letter_score(filename):
             letter_score[int(formated[0])] = formated[1:]
         return letter_score
 
+def read_words(filename):
+    with open(words_filename) as file_object:
+        contents = strip_accents(file_object.read())
+    words = contents.lower().replace('"', '').replace(',', '').replace('\n', ' ').split()
+    return words
+
 def write_letter_score(filename, letter_score):
     with open(filename, 'w') as file_object:
         json.dump(letter_score, file_object)
 
+def write_words(filename, words):
+    with open(json_filename, 'w') as file_object:
+        json.dump(words,file_object)
+
 letter_score = read_letter_score(letters_filename)
 write_letter_score(letters_json_filename, letter_score)
 
-with open(words_filename) as file_object:
-    contents = file_object.read()
-
-words = contents.lower().replace('"', '').replace(',', '').replace('\n', ' ').split()
-
-with open(json_filename, 'w') as file_object:
-    json.dump(words,file_object)
-
-# dicionario = { 1: ['a','f'], 2: 'b'}
-# print(dicionario[1])
+words = read_words(words_filename)
+write_words(json_filename, words)
