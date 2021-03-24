7e11e6f350c9897deb23647cc398fc6f96db722f
fmateus <fm4teus@gmail.com>
2021-03-24 20:28:35 -0300

    get word list sorted by score

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ words_filename = 'words.txt'
 json_filename = 'words_db.json'
 letters_json_filename = 'letters_db.json'
 
+
 def read_letter_score(filename):
     letter_score = {}
     with open(filename) as file_object:
         contents = file_object.readlines()
         for line in contents:
-            formated = line.lower().strip().split()
+            formated = line.lower().replace(',', '').strip().split()
             del formated[1]
-            letter_score[int(formated[0])] = formated[1:]
+            letter_score[formated[0]] = formated[1:]
         return letter_score
 
 def read_words(filename):
@@ -34,8 +35,31 @@ def write_words(filename, words):
     with open(json_filename, 'w') as file_object:
         json.dump(words,file_object)
 
+def get_word_score(word, letter_score):
+    list_word = list(word)
+    word_score = 0
+    for letter_word in list_word:
+        for score, letters in letter_score.items():
+            if letter_word in letters:
+                word_score += int(score)
+    return word_score
+
+def get_word_dict_score(word):
+    return word['score']
+
 letter_score = read_letter_score(letters_filename)
 write_letter_score(letters_json_filename, letter_score)
 
 words = read_words(words_filename)
 write_words(json_filename, words)
+
+words_dicts = []
+
+for word in words:
+    words_dicts.append({ 'word': word, 'score': get_word_score(word, letter_score) })
+
+words_dicts.sort(key=get_word_dict_score, reverse=True)
+
+for words_dict in words_dicts:
+    print(words_dict['word'] + ': ' + str(words_dict['score']))
+
